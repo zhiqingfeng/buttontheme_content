370b215ab169f99e1f768818e96ed68ed9d1617e
juhahinkula <hinkula@gmail.com>
2020-08-07 13:55:12 +0300

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderButton from './HeaderButton';
+
+function Header() {
+  return (
+    <div>
+      <HeaderButton />
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/HeaderButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HeaderButton(props) {
+  return (
+    <div>
+      <button>Press me</button>
+    </div>
+  );
+}
+
+export default HeaderButton;
